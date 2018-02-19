04ca6484db12ca9f168a00d6c3d5a47ca70d76e6
doiken23 <doi-kento-astro@g.ecc.u-tokyo.ac.jp>
2018-02-19 17:18:31 +0900

    add large autoencoder

--- autoencoder.py
+++ autoencoder.py
@@ -34,3 +34,34 @@ class AutoEncoder(nn.Module):
         decoded = self.decoder(encoded)
 
         return encoded, decoded
+
+class Large_AutoEncoder(nn.Module):
+    def __init__(self, input_size, embedding_dimension):
+        # initialization of class
+        super(Large_AutoEncoder, self).__init__()
+
+        # define the network
+        self.encoder = nn.Sequential(
+                        nn.Linear(input_size, 1000),
+                        nn.ReLU(),
+                        nn.Linear(1000, 1000),
+                        nn.ReLU(),
+                        nn.Linear(1000, 4000),
+                        nn.ReLU(),
+                        nn.Linear(4000, embedding_dimension)
+                        )
+        self.decoder = nn.Sequential(
+                        nn.Linear(embedding_dimension, 4000),
+                        nn.ReLU(),
+                        nn.Linear(4000, 2000),
+                        nn.ReLU(),
+                        nn.Linear(2000, 2000),
+                        nn.ReLU(),
+                        nn.Linear(2000, input_size)
+                        )
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+
+        return encoded, decoded
