bdce0f681f4092b37a88e16183a2c0ac26b22221
Doi Kento <doi-kento-astro@g.ecc.u-tokyo.ac.jp>
2019-02-02 17:20:13 +0900

    [update] add losse

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
 from .dcgan import Generator, Discriminator
 from .sat_dataset import SATDataset
 from .sngan import SNGANProjectionDiscriminator, SNGANGenerator
+from src.losses import DisLoss, GenLoss
